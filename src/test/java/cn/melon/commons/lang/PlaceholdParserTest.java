3636b4f846f90d98ef062e1137d8cb563d355a43
imelonkid <imelonkid@163.com>
2021-08-07 13:54:22 +0800

    修改测试用力

--- src/test/java/cn/melon/commons/lang/PlaceholdParserTest.java
+++ src/test/java/cn/melon/commons/lang/PlaceholdParserTest.java
@@ -27,17 +27,12 @@ public class PlaceholdParserTest {
 
   @Test
   public void testParseAndReplaceByName(){
-    String testStr = "this is a test, h1:{} h2:${test} h2:${test} h3{";
+    String testStr = "this is a test, h1:{} h2:${test} h3:${test1} h4:${} h5:${test2} h6:${";
     Map<String, String> params = new HashMap<>();
     params.put("test", "melonkid");
+    params.put("test2", "");
     String finalStr = PlaceholdParser.parseAndReplaceByName(testStr, params);
-    assert "this is a test, h1:{} h2:melonkid h2:melonkid h3{".equals(finalStr);
-
-    String testStr1 = "this is a test, h1:{} h2:${test} h2:${test} h3:${";
-    params.put("test", "melonkid");
-    String finalStr1 = PlaceholdParser.parseAndReplaceByName(testStr1, params);
-    System.out.println(finalStr1);
-    assert "this is a test, h1:{} h2:melonkid h2:melonkid h3:${".equals(finalStr1);
+    assert "this is a test, h1:{} h2:melonkid h3:${test1} h4:${} h5: h6:${".equals(finalStr);
   }
 
 
